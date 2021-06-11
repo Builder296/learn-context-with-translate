89e6c81afbf35fe12c7c5d33e712f53cd238b201
builder296 <kessirin296@gmail.com>
2021-06-11 16:36:32 +0700

    Refactor on submit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,17 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext.js';
 
 class Button extends React.Component {
-  static contextType = LanguageContext; // connect to Context
+  static contextType = LanguageContext;
+
+  renderSunmit(value) {
+    return value === 'english' ? ' Sumit' : 'Voorleggen'
+  }
 
   render() {
     // const text = this.context === 'english' ? ' Sumit' : 'Voorleggen';
     return <button className="ui button primary">
       <LanguageContext.Consumer>
-        {(value) => value === 'english' ? ' Sumit' : 'Voorleggen'}
+        {(value) => this.renderSunmit(value)}
       </LanguageContext.Consumer>
     </button>;
   }
