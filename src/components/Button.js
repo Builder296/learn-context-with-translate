48080e7399f4c15d86416f8cdc6dc1586fc7d9f9
builder296 <kessirin296@gmail.com>
2021-06-11 16:44:05 +0700

    User color context with button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import LanguageContext from '../contexts/LanguageContext.js';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  static contextType = LanguageContext;
-
   renderSunmit(value) {
-    return value === 'english' ? ' Sumit' : 'Voorleggen'
+    return value === 'english' ? ' Sumit' : 'Voorleggen';
   }
 
   render() {
-    // const text = this.context === 'english' ? ' Sumit' : 'Voorleggen';
-    return <button className="ui button primary">
-      <LanguageContext.Consumer>
-        {(value) => this.renderSunmit(value)}
-      </LanguageContext.Consumer>
-    </button>;
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => this.renderSunmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
 
