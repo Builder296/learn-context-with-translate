f39ae26f7ba572590adb8f84d88b7b357712785d
builder296 <kessirin296@gmail.com>
2021-06-11 15:52:26 +0700

    context

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,4 +1,18 @@
 import React from 'react';
 
-// createContext with default value
 export default React.createContext('english');
+
+// console.log(context);
+// Object
+// $$typeof: Symbol(react.context)
+// Consumer: {$$typeof: Symbol(react.context), _context: {…}, _calculateChangedBits: null, …}
+// Provider: {$$typeof: Symbol(react.provider), _context: {…}}
+// _calculateChangedBits: null
+// _currentRenderer: {}
+// _currentRenderer2: null
+// _currentValue: "english"
+// _currentValue2: "english"
+// _threadCount: 0
+// __proto__: Object
+
+// export default context;
